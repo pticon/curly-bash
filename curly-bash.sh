0850548e16cbf4c30fdbec6d32752e7168762be5
Pierre Lebleu <pierre.lebleu@technicolor.com>
2016-09-07 16:53:40 +0200

    top10 and color man pages

--- curly-bash.sh
+++ curly-bash.sh
@@ -197,6 +197,30 @@ function only()
 	fi
 }
 
+function top10()
+{
+	history | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' \
+		| grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n10
+}
+
+
+# Color man pages
+#export LESS_TERMCAP_mb=$(printf '\e[01;31m') # enter blinking mode - red
+#export LESS_TERMCAP_md=$(printf '\e[01;35m') # enter double-bright mode - bold, magenta
+#export LESS_TERMCAP_me=$(printf '\e[0m') # turn off all appearance modes (mb, md, so, us)
+#export LESS_TERMCAP_se=$(printf '\e[0m') # leave standout mode
+#export LESS_TERMCAP_so=$(printf '\e[01;33m') # enter standout mode - yellow
+#export LESS_TERMCAP_ue=$(printf '\e[0m') # leave underline mode
+#export LESS_TERMCAP_us=$(printf '\e[04;36m') # enter underline mode - cyan
+
+export LESS_TERMCAP_mb=$(printf '\e[01;31m') # enter blinking mode
+export LESS_TERMCAP_md=$(printf '\e[01;38;5;75m') # enter double-bright mode
+export LESS_TERMCAP_me=$(printf '\e[0m') # turn off all appearance modes (mb, md, so, us)
+export LESS_TERMCAP_se=$(printf '\e[0m') # leave standout mode
+export LESS_TERMCAP_so=$(printf '\e[01;33m') # enter standout mode
+export LESS_TERMCAP_ue=$(printf '\e[0m') # leave underline mode
+export LESS_TERMCAP_us=$(printf '\e[04;38;5;200m') # enter underline mode
+
 
 # PS1
 if [ $(/usr/bin/whoami) = 'root' ]; then
