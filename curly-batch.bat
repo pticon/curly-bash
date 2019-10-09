73a61873396fdb1c5ad2b93d4fd49b9f590604a9
Pierre Lebleu <pme.lebleu@gmail.com>
2019-10-09 13:07:05 +0200

    cury-batch: add the batch file

---
+++ curly-batch.bat
@@ -0,0 +1,9 @@
+REM custom settings for windows shell
+
+REM set some macros/aliases
+DOSKEY ls=dir /B $*
+DOSKEY l=dir /B $*
+DOSKEY edit="C:\Program Files (x86)\Notepad++\notepad++.exe" $*
+DOSKEY s=cd ..
+DOSKEY cp=copy $*
+DOSKEY vi=vim $*
